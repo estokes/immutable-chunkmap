200e07e7e577edc5e9b36a7775036aa435715633
Eric Stokes <letaris@gmail.com>
2017-10-12 13:23:29 -0400

    Revert "balance compiles"
    This reverts commit 4385cc32b540af6bb95cbb081a7acc75ce0b9cd3.

--- src/lib.rs
+++ src/lib.rs
@@ -26,13 +26,13 @@ fn em<K: Ord, V>() -> Rc<Tree<K,V>> {
   Rc::new(Tree::Empty)
 }
 
-pub fn singleton<K: Ord, V>(k: Rc<K>, v: Rc<V>) -> Rc<Tree<K, V>> {
+pub fn singleton<K: Ord, V>(k: &Rc<K>, v: &Rc<V>) -> Rc<Tree<K, V>> {
   Rc::new(Tree::Leaf(k.clone(), v.clone()))
 }
 
-pub fn create<K: Ord, V>(l: Rc<Tree<K,V>>,
-                         k: Rc<K>, v: &Rc<V>,
-                         r: Rc<Tree<K,V>>)
+pub fn create<K: Ord, V>(l: &Rc<Tree<K,V>>,
+                         k: &Rc<K>, v: &Rc<V>,
+                         r: &Rc<Tree<K,V>>)
                          -> Rc<Tree<K,V>> {
   let (hl, hr) = (height(l), height(r));
   if hl == 0 && hr == 0 { Rc::new(Tree::Leaf(k.clone(), v.clone())) }
@@ -46,42 +46,42 @@ pub fn create<K: Ord, V>(l: Rc<Tree<K,V>>,
   }
 }
 
-fn bal<K: Ord, V>(l: Rc<Tree<K,V>>, x: Rc<K>, d: Rc<V>, r: Rc<Tree<K,V>>)
+fn bal<K: Ord, V>(l: &Rc<Tree<K,V>>, x: &Rc<K>, d: &Rc<V>, r: &Rc<Tree<K,V>>)
                   -> Rc<Tree<K,V>>  {
   let hl = height(l);
   let hr = height(r);
   if hl > hr + 2 {
-    match *l {
+    match **l {
       Tree::Empty | Tree::Leaf(_, _) => panic!("tree heights are wrong"),
       Tree::Node(Node {left:ref ll, key:ref lv, val:ref ld, right:ref lr, ..}) => {
         if height(ll) >= height(lr) {
-          create(ll, lv, ld, create(lr, x, d, r))
+          create(ll, lv, ld, &create(lr, x, d, r))
         } else {
-          match *lr {
+          match **lr {
             Tree::Empty => panic!("tree heights wrong"),
             Tree::Leaf(ref lrv, ref lrd) =>
-              create(create(ll, lv, ld, em()), lrv, lrd, create(em(), x, d, r)),
+              create(&create(ll, lv, ld, &em()), lrv, lrd, &create(&em(), x, d, r)),
             Tree::Node(Node {left:ref lrl, key: ref lrv,
                              val: ref lrd, right: ref lrr, ..}) =>
-              create(create(ll, lv, ld, lrl), lrv, lrd, create(lrr, x, d, r))
+              create(&create(ll, lv, ld, lrl), lrv, lrd, &create(lrr, x, d, r))
           }
         }
       }
     }
   } else if hr > hl + 2 {
-    match *r {
+    match **r {
       Tree::Empty | Tree::Leaf(_,_) => panic!("tree heights are wrong"),
       Tree::Node(Node {left:ref rl, key: ref rv, val: ref rd, right:ref rr, ..}) => {
         if height(rr) >= height(rl) {
-          create(create(l, x, d, rl), rv, rd, rr)
+          create(&create(l, x, d, rl), rv, rd, rr)
         } else {
-          match *rl {
+          match **rl {
             Tree::Empty => panic!("tree heights are wrong"),
             Tree::Leaf(ref rlv, ref rld) =>
-              create(create(l, x, d, em()), rlv, rld, create(em(), rv, rd, rr)),
+              create(&create(l, x, d, &em()), rlv, rld, &create(&em(), rv, rd, rr)),
             Tree::Node(Node {left: ref rll, key: ref rlv,
                              val: ref rld, right:ref rlr, ..}) =>
-              create(create(l, x, d, rll), rlv, rld, create(rlr, rv, rd, rr))
+              create(&create(l, x, d, rll), rlv, rld, &create(rlr, rv, rd, rr))
           }
         }
       }
@@ -90,13 +90,6 @@ fn bal<K: Ord, V>(l: Rc<Tree<K,V>>, x: Rc<K>, d: Rc<V>, r: Rc<Tree<K,V>>)
     create(l, x, d, r)
   }
 }
-
-/*
-fn add<K: Ord, V>(t: Rc<Tree<K,V>>, length:u64, k: &Rc<K>, v: &Rc<V>)
-                  -> (Rc<Tree<K,V>>, u64) {
-  
-}
-*/
 
 #[cfg(test)]
 mod tests {
