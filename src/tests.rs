91643d3cd70b40155631ea811ba15d36ded1a5df
Eric Stokes <letaris@gmail.com>
2017-10-14 16:58:25 -0400

    adding more tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,11 @@
 extern crate rand;
 use avl;
-use std::iter::{Iterator, IntoIterator};
+use tests::rand::{random, Rand};
+use std::iter::{IntoIterator};
+use std::vec::{Vec};
 
-fn add<I, T>(r: I) -> avl::Tree<T, T>
-  where I: Iterator<Item=T>, T: Ord + Clone
+fn add<I, T>(r: I) -> (avl::Tree<T, T>, usize)
+  where I: IntoIterator<Item=T>, T: Ord + Clone
 {
   let mut t = avl::empty();
   let mut len = 0;
@@ -13,24 +15,43 @@ fn add<I, T>(r: I) -> avl::Tree<T, T>
     len = tlen;
     avl::invariant(&t, Option::Some(len));
   }
-  t
+  (t, len)
+}
+
+fn randvec<T: Rand>(len: usize) -> Vec<T> {
+  let mut v: Vec<T> = Vec::new();
+  for _ in 0..len { v.push(random()) }
+  v
 }
 
 #[test]
 fn test_add_int_seq_asc() {
-  add(1..10000); ()
+  let (_, len) = add(0..10000);
+  if len != 10000 { panic!("length is wrong expected 10000 got {}", len) }
 }
 
 #[test]
 fn test_add_int_seq_dec() {
-  add(10000..1); ()
+  let (_, len) = add((0..10000).rev());
+  if len != 10000 {panic!("length is wrong expected 10000 got {}", len)}
 }
 
 #[test]
 fn test_add_int_rand() {
-  let v = Vec<int32>::new(10000);
-  for mut i in v {
-    i = rand::random<int32>();
+  add(randvec::<i32>(10000)); ()
+}
+
+#[test]
+fn test_find_int_rand() {
+  let v = randvec::<i32>(10000);
+  let (t, _) = add(&v);
+  for k in &v {
+    match avl::find(&t, &k) {
+      Option::None => panic!("key {} was added but could not be found", k),
+      Option::Some(v) =>
+        if *v != k {
+          panic!("key {} was added, and found, but has the wrong value {}", k, v)
+        }
+    }
   }
-  add(v.into_iterator()); ()
 }
