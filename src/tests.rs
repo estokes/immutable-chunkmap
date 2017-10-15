46f17c8e7ba7a9c0909966d83259913bdd9e8452
Eric Stokes <letaris@gmail.com>
2017-10-15 16:04:36 -0400

    tune tests so they take a reasonable amount of time. fix bug in map test

--- src/tests.rs
+++ src/tests.rs
@@ -14,7 +14,7 @@ fn add<I, T>(r: I) -> (avl::Tree<T, T>, usize)
     let (tt, tlen) = avl::add(&t, len, &i, &i);
     t = tt;
     len = tlen;
-    avl::invariant(&t, Option::Some(len));
+    avl::invariant(&t, len);
   }
   (t, len)
 }
@@ -53,42 +53,38 @@ fn test_find_int_rand() {
 
 #[test]
 fn test_int_add_remove_rand() {
-  let v = randvec::<i32>(10000);
+  let v = randvec::<i32>(7000);
   let (mut t, mut len) = add(&v);
   for k in &v {
     assert_eq!(*avl::find(&t, &k).unwrap(), k);
     let (tt, tlen) = avl::remove(&t, len, &k);
     t = tt;
     len = tlen;
-    avl::invariant(&t, Option::Some(len));
+    avl::invariant(&t, len);
     assert_eq!(avl::find(&t, &k), Option::None);
   }
 }
 
 #[test]
 fn test_int_map_rand() {
-  let v = randvec::<i32>(10000);
+  let v = randvec::<i32>(4000);
   let mut t = map::empty();
   let mut i = 0;
   for k in &v {
     t = map::add(&t, &k, &k);
-    map::invariant(t);
+    map::invariant(&t);
     i = i + 1;
     for k in &v[0..i] { 
-      assert_eq!(*map::find(&t, &k).unwrap(), *k)
+      assert_eq!(*map::find(&t, &k).unwrap(), k)
     }
   }
   i = 0;
   for k in &v {
     t = map::remove(&t, &k);
+    map::invariant(&t);
     i = i + 1;
-    let mut j = 0;
-    for k in &v {
-      if j < i {
-        assert_eq!(map::find(&t, &k), Option::None)
-      } else {
-        assert_eq!(*map::find(&t, &k).unwrap(), &k)
-      }
+    for k in &v[0..i] {
+      assert_eq!(map::find(&t, &k), Option::None)
     }
   }
 }
