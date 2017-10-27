966388c98b8418b630612f8ab3f91d468e0beaea
Eric Stokes <letaris@gmail.com>
2017-10-27 13:24:34 -0400

    add bench for stock rust map

---
+++ bench_imap/target/debug/bench_imap.d
@@ -0,0 +1,1 @@
+/home/eric/Projects/rust-immutable-avl/bench_imap/target/debug/bench_imap: /home/eric/Projects/rust-immutable-avl/bench_imap/src/main.rs
---
+++ bench_imap/target/release/bench_imap.d
@@ -0,0 +1,1 @@
+/home/eric/Projects/rust-immutable-avl/bench_imap/target/release/bench_imap: /home/eric/Projects/rust-immutable-avl/bench_imap/src/main.rs
