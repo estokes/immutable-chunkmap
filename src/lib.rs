020652fc6dada4518eeb19692b235bb863032aa7
Eric Stokes <letaris@gmail.com>
2017-12-15 18:00:06 -0500

    tune sizes

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod macros;
 
 pub mod rc {
   pub(crate) mod avl {
-    avltree!(std::rc::Rc, Rc, Rc::new, 512);
+    avltree!(std::rc::Rc, Rc, Rc::new, 1132);
   }
 
   pub mod map {
@@ -13,7 +13,7 @@ pub mod rc {
 
 pub mod arc {
   pub(crate) mod avl {
-    avltree!(std::sync::Arc, Arc, Arc::new, 512);
+    avltree!(std::sync::Arc, Arc, Arc::new, 1132);
   }
 
   pub mod map {
