c83561227d66a9bbc4cbb9e524feb5378fc88737
Eric Stokes <letaris@gmail.com>
2017-12-25 15:28:05 -0500

    change run script to be csv friendly

--- bench-ocaml/run.sh
+++ bench-ocaml/run.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 for i in 1000 10000 100000 1000000 10000000; do
-  echo "size $i"
   for j in 1 2 3; do
     ./test $i
   done
--- bench/run.sh
+++ bench/run.sh
@@ -5,7 +5,6 @@ B=target/release/bench
 function run() {
   KIND="$1"
   SIZE="$2"
-  echo "$KIND at $SIZE"
   for k in 1 2 3; do
     if ! test "$KIND" == "ls" -a "$SIZE" -gt 100000; then
       $B $KIND $SIZE
@@ -13,9 +12,9 @@ function run() {
   done
 }
 
-# std avl
-for i in 1000 10000 100000 1000000 10000000; do
-  for j in avl rc btm hm bs ls; do
+for j in avl rc btm hm bs ls; do
+  echo "testing $j"
+  for i in 1000 10000 100000 1000000 10000000; do
     run $j $i
   done
 done
