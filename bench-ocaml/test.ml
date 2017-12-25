e6dcfce803af79cf6f6aa44896c91549c7830d6c
Eric Stokes <letaris@gmail.com>
2017-12-25 14:45:39 -0500

    add ocaml test

---
+++ bench-ocaml/test.ml
@@ -0,0 +1,47 @@
+open Core
+
+let random_array size = Array.init size (fun _ -> Random.int Int.max_value)
+
+let bench_add v =
+  let st = Time.now () in
+  let m =
+    Array.fold v ~init:(Map.empty (module Int)) ~f:(fun m k ->
+      Map.set m ~key:k ~data:k)
+  in
+  let en = Time.now () in
+  (m, Time.diff en st)
+
+
+
+let bench_find m v =
+  let st = Time.now () in
+  for i = 0 to Array.length v - 1 do
+    let k = Array.unsafe_get v i in
+    assert (Option.value_exn (Map.find m k) = k)
+  done;
+  let en = Time.now () in
+  Time.diff en st
+
+let bench_remove m v =
+  let st = Time.now () in
+  let m =
+    Array.fold v ~init:m ~f:(fun m k ->
+      Map.remove m k)
+  in
+  if Map.length m <> 0 then failwith "remove is broken";
+  let en = Time.now () in
+  Time.diff en st
+
+let () =
+  let size =
+    if Array.length Sys.argv = 2 then Int.of_string Sys.argv.(1)
+    else 10000
+  in
+  let v = random_array size in
+  let (m, add) = bench_add v in
+  Gc.compact ();
+  let find = bench_find m v in
+  let rm = bench_remove m v in
+  let str t = sprintf "%gns" (Time.Span.to_ns t /. float size) in
+  printf "add: %s, find: %s, remove: %s\n%!"
+    (str add) (str find) (str rm)
