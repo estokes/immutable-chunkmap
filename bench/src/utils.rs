497f5b55de2f13633de23d28c518ab032ee7ac48
Eric Stokes <letaris@gmail.com>
2019-11-03 19:56:57 -0500

    remove old benchmark files

--- bench/src/utils.rs
+++ bench/src/utils.rs
@@ -1,9 +1,10 @@
-use rand::{
-    SeedableRng, Rng,
-    rngs::StdRng,
-    distributions::{Distribution, Standard},
+use rand::Rng;
+use std::{
+    time::Duration,
+    sync::Arc,
 };
-use std::time::Duration;
+
+const STRSIZE: usize = 16;
 
 pub(crate) trait Rand: Sized {
     fn rand<R: Rng>(r: &mut R) -> Self;
@@ -50,13 +51,6 @@ pub(crate) fn randvec<T: Rand>(len: usize) -> Vec<T> {
     v
 }
 
-pub(crate) fn permutation<T: Clone>(v: &Vec<T>) -> Vec<T> {
-    let p = randvec::<usize>(v.len());
-    let mut p = p.iter().zip(v).collect::<Vec<_>>();
-    p.sort_by(|(k0, _), (k1, _)| k0.cmp(k1));
-    p.into_iter().map(|(_, v)| v.clone()).collect::<Vec<T>>()
-}
-
 #[allow(dead_code)]
 pub(crate) fn to_ms(t: Duration) -> u64 {
     t.as_secs() * 1000 + ((t.subsec_nanos() / 1000000) as u64)
@@ -73,14 +67,4 @@ pub(crate) fn to_ns(t: Duration) -> u64 {
 
 pub(crate) fn to_ns_per(t: Duration, n: usize) -> f64 {
     (to_ns(t) as f64) / (n as f64)
-}
-
-pub(crate) fn permute<T: Clone>(v: &Vec<T>) -> Vec<T> {
-    let pos = randvec::<usize>(v.len());
-    let mut res = Vec::new();
-    for i in 0..v.len() {
-        res.push((pos[i], v[i].clone()));
-    }
-    res.sort_unstable_by_key(|&(k, _)| k);
-    res.iter().map(|&(_, ref v)| v.clone()).collect()
 }
