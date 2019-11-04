72b5f3303872d685b9b3c5e97d332b36d933fb47
Eric Stokes <letaris@gmail.com>
2019-11-03 19:09:46 -0500

    improve command line args

--- bench/src/main.rs
+++ bench/src/main.rs
@@ -9,21 +9,21 @@ mod gen;
 use gen::{Bench, CMWrap};
 
 fn usage() {
-    println!("usage: <cm_ptr|cm_str|btm_ptr|btm_str|hm_ptr|hm_str> <size>")
+    println!("usage: <cm|btm|hm> <ptr|str> <size>")
 }
 
 fn main() {
     let args = Vec::from_iter(env::args());
-    if args.len() != 3 { usage() }
+    if args.len() != 4 { usage() }
     else {
-        let size = args[2].parse::<usize>().unwrap();
-        match args[1].as_ref() {
-            "cm_ptr" => Bench<CMWrap<usize, usize>>::run(size),
-            "cm_str" => Bench<CMWrap<Arc<String>, Arc<String>>::run(size),
-            "btm_ptr" => Bench<BTreeMap<usize, usize>>::run(size),
-            "btm_str" => Bench<BTreeMap<Arc<String>, Arc<String>>>::run(size),
-            "hm_ptr" => Bench<HashMap<usize, usize>>::run(size),
-            "hm_str" => Bench<HashMap<Arc<String>, Arc<String>>>::run(size),
+        let size = args[3].parse::<usize>().unwrap();
+        match (&args[1], &args[2]) {
+            ("cm", "ptr") => Bench<CMWrap<usize, usize>>::run(size),
+            ("cm", "str") => Bench<CMWrap<Arc<String>, Arc<String>>::run(size),
+            ("btm", "ptr") => Bench<BTreeMap<usize, usize>>::run(size),
+            ("btm", "str") => Bench<BTreeMap<Arc<String>, Arc<String>>>::run(size),
+            ("hm", "ptr") => Bench<HashMap<usize, usize>>::run(size),
+            ("hm", "str") => Bench<HashMap<Arc<String>, Arc<String>>>::run(size),
             _ => usage() 
         }
     }
