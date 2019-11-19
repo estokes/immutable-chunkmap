5feab2ef0b610ccf5e7c3dba7df972aa6e3fcc1d
Eric Stokes <letaris@gmail.com>
2019-11-18 21:55:56 -0500

    finished compare script

--- bench/compare.py
+++ bench/compare.py
@@ -11,7 +11,7 @@ def compare(k, res0, res1, cmp0, cmp1):
     dat0 = res0[cmp0]
     dat1 = res1[cmp1]
     n = 1000;
-    for i in range(0, dat0[k].length()):
+    for i in range(0, 5):
         d0 = dat0[k]
         d1 = dat1[k]
         insert = d1['insert'][i] / d0['insert'][i]
@@ -20,7 +20,7 @@ def compare(k, res0, res1, cmp0, cmp1):
         get = d1['get'][i] / d0['get'][i]
         get_parallel = d1['get_parallel'][i] / d0['get_parallel'][i]
         remove = d1['remove'][i] / d0['remove'][i]
-        print(f'{n},{insert},{insert_many},{insert_many_par},{get},{get_parallel},{remove}')
+        print(f'{n:8},{insert:.2f},{insert_many:.2f},{insert_many_par:.2f},{get:.2f},{get_parallel:.2f},{remove:.2f}')
         n = n*10
     
 parser = argparse.ArgumentParser(description = "compare benchmarks")
@@ -31,5 +31,5 @@ args = parser.parse_args()
 res0 = load_results(args.data[0])
 res1 = load_results(args.data[1])
 
-compare('ptr', res0[args.compare[0]], res1[args.compare[1]])
-compare('str', res0[args.compare[0]], res1[args.compare[1]])
+compare('ptr', res0, res1, args.compare[0], args.compare[1])
+compare('str', res0, res1, args.compare[0], args.compare[1])
