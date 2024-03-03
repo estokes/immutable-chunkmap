dba1d83934bccef034a2b4cdcf78d6b81ac53a61
Matt Campbell <mattcampbell@pobox.com>
2024-03-03 09:02:42 -0600

    Remove old pre-built docs

--- doc/immutable_chunkmap/set/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Set","This set uses a similar strategy to BTreeSet to ensure cache efficient performance on modern hardware while still providing log(N) get, insert, and remove operations. # Examples ``` use std::string::String; use self::immutable_chunkmap::set::Set;"],["SetIter",""]]});
